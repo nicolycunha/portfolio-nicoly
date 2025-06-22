80b5b65f9c9f7ad1d13d5898940816df44bb16c5
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-06-22 18:31:13 -0300

    feat: create favicon

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="flex flex-col items-center text-muted-foreground py-4 text-sm">
+    <div className="flex flex-col items-center text-muted-foreground pb-4 pt-10 text-sm">
       Copyright © {new Date().getFullYear()} Nicoly Cunha. Todos os direitos
       reservados.
     </div>
